b26d9031b6884714b658ca91be2a051ed9c6fa67
Guy Messika <guy032@gmail.com>
2020-10-25 09:39:01 +0200

    sync

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ set -e
 
 apt-get update -y
 apt-get install -y curl
+apt-get install -y libjbig-dev
+apt-get install -y libtiff-dev
 
 # listen on all interfaces, allow connections from anywhere
 sed -i -e "s/name=\"listen-ip\" value=\".*\"/name=\"listen-ip\" value=\"0.0.0.0\"/g" /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml
